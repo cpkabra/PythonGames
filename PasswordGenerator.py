aff4eeddfafa259e431e0cb9174bcb8f967c1727
Chandraprakash Kabra <chandraprakashkabra@gmail.com>
2020-04-24 11:20:59 +0530

    PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+size = int(input("Please enter the length for the password: "))
+password_chars = string.ascii_letters + string.digits + string.punctuation
+generated_pass = ""
+for i in range(size):
+    generated_pass += random.choice(password_chars)
+print("Random password: " + generated_pass)
